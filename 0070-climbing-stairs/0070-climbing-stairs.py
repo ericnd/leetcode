cf230be3256d38821e62b4af1ff913fc41e5d529
Eric <47131649+ericnd@users.noreply.github.com>
2024-04-07 20:01:23 +1000

    Time: 34 ms (59.39%), Space: 16.6 MB (31.08%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = {}
+        
+        def climb(s):
+            ways = 0
+            if s in dp:
+                return dp[s]
+            
+            if s <= 3:
+                return s
+            
+            dp[s] = climb(s - 1) + climb(s - 2)
+            return climb(s - 1) + climb(s - 2)
+        
+        return climb(n)
